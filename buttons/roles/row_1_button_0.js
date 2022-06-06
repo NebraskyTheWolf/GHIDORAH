9d90988d73ec051b151b05734252444fa40686f3
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-06 20:28:37 +0200

    Self roles beta-code upgrade

---
+++ buttons/roles/row_1_button_0.js
@@ -0,0 +1,19 @@
+const { MessageMenuOption, MessageMenu  } = require('discord-buttons');
+module.exports = {
+    data: {
+        name: "row_1_button_0"
+    },
+    async execute(interaction, interactionUser, guild) {
+        interaction.reply({
+            "embeds": [
+                {
+                  "type": "rich",
+                  "title": `Role informations`,
+                  "description": `Here you can choice your roles in the differents category.\nPlease do not abuse with the roles take only the roles are made for you thanks.\n\nif you want a custom roles or asking new roles please contact us :3`,
+                  "color": 0xff0084
+                }
+            ],
+            "ephemeral": true
+        });
+    }
+}
