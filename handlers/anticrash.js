7d13c8e88456dfab7fdb8d3526959df175005aa9
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-08 16:58:13 +0200

    Error logger fix 3

--- handlers/anticrash.js
+++ handlers/anticrash.js
@@ -1,99 +1,105 @@
 module.exports = (client) => {
-    const channel = client.guilds.cache.get('917714328327692338').channels.cache.get('984100098487230504');
+    let channel;
+    if (client.IsLoaded) {
+        channel = client.guilds.cache.get('917714328327692338').channels.cache.get('984100098487230504');
+    }
     let errorId = 0;
     process.on("unhandledRejection", async (reason, p) => {
         errorId++;
         if (client.IsDebug)
             client.logger.log('ERROR', `${reason}, ${p}`);
         
-        
-        channel.send({
-            "components": [
-                {
-                  "type": 1,
-                  "components": [
+        if (client.IsLoaded) {
+            channel.send({
+                "components": [
                     {
-                      "style": 4,
-                      "label": `RESTART NOW`,
-                      "custom_id": `row_reload`,
-                      "disabled": false,
-                      "type": 2
+                    "type": 1,
+                    "components": [
+                        {
+                        "style": 4,
+                        "label": `RESTART NOW`,
+                        "custom_id": `row_reload`,
+                        "disabled": false,
+                        "type": 2
+                        }
+                    ]
                     }
-                  ]
-                }
-              ],
-              "embeds": [
-                {
-                  "type": "rich",
-                  "title": `unhandledRejection #${errorId}`,
-                  "description": `ERROR: \n\n \`\`\` ${reason} \`\`\` \n\nORIGIN:\n\n \`\`\` ${p} \`\`\` \n\nSERVICE:\n\n \`\`\` GHIDORAH \`\`\``,
-                  "color": 0xff003c,
-                  "fields": [
+                ],
+                "embeds": [
                     {
-                      "name": `VERSION`,
-                      "value": `5.2.3`,
-                      "inline": true
-                    },
-                    {
-                      "name": `NODEJS`,
-                      "value": `v${process.version}`,
-                      "inline": true
-                    },
-                    {
-                      "name": `API STATE`,
-                      "value": `ALIVE_ACK`,
-                      "inline": true
+                    "type": "rich",
+                    "title": `unhandledRejection #${errorId}`,
+                    "description": `ERROR: \n\n \`\`\` ${reason} \`\`\` \n\nORIGIN:\n\n \`\`\` ${p} \`\`\` \n\nSERVICE:\n\n \`\`\` GHIDORAH \`\`\``,
+                    "color": 0xff003c,
+                    "fields": [
+                        {
+                        "name": `VERSION`,
+                        "value": `5.2.3`,
+                        "inline": true
+                        },
+                        {
+                        "name": `NODEJS`,
+                        "value": `v${process.version}`,
+                        "inline": true
+                        },
+                        {
+                        "name": `API STATE`,
+                        "value": `ALIVE_ACK`,
+                        "inline": true
+                        }
+                    ]
                     }
-                  ]
-                }
-            ]
-        });
+                ]
+            });
+        }
     });
     process.on("uncaughtException", (err, origin) => {
         errorId++;
         if (client.IsDebug)
             client.logger.log('ERROR', `${err}, ${origin}`);
         
-        channel.send({
-            "components": [
-                {
-                  "type": 1,
-                  "components": [
+        if (client.IsLoaded) {
+            channel.send({
+                "components": [
                     {
-                      "style": 4,
-                      "label": `RESTART NOW`,
-                      "custom_id": `row_reload`,
-                      "disabled": false,
-                      "type": 2
+                    "type": 1,
+                    "components": [
+                        {
+                        "style": 4,
+                        "label": `RESTART NOW`,
+                        "custom_id": `row_reload`,
+                        "disabled": false,
+                        "type": 2
+                        }
+                    ]
                     }
-                  ]
-                }
-              ],
-              "embeds": [
-                {
-                  "type": "rich",
-                  "title": `unhandledRejection #${errorId}`,
-                  "description": `ERROR: \n\n \`\`\` ${err} \`\`\` \n\nORIGIN:\n\n \`\`\` ${origin} \`\`\` \n\nSERVICE:\n\n \`\`\` GHIDORAH \`\`\``,
-                  "color": 0xff003c,
-                  "fields": [
+                ],
+                "embeds": [
                     {
-                      "name": `VERSION`,
-                      "value": `5.2.3`,
-                      "inline": true
-                    },
-                    {
-                      "name": `NODEJS`,
-                      "value": `v${process.version}`,
-                      "inline": true
-                    },
-                    {
-                      "name": `API STATE`,
-                      "value": `ALIVE_ACK`,
-                      "inline": true
+                    "type": "rich",
+                    "title": `unhandledRejection #${errorId}`,
+                    "description": `ERROR: \n\n \`\`\` ${err} \`\`\` \n\nORIGIN:\n\n \`\`\` ${origin} \`\`\` \n\nSERVICE:\n\n \`\`\` GHIDORAH \`\`\``,
+                    "color": 0xff003c,
+                    "fields": [
+                        {
+                        "name": `VERSION`,
+                        "value": `5.2.3`,
+                        "inline": true
+                        },
+                        {
+                        "name": `NODEJS`,
+                        "value": `v${process.version}`,
+                        "inline": true
+                        },
+                        {
+                        "name": `API STATE`,
+                        "value": `ALIVE_ACK`,
+                        "inline": true
+                        }
+                    ]
                     }
-                  ]
-                }
-            ]
-        });
+                ]
+            });
+        }
     });
 }
