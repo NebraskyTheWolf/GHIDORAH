5c2d8278b85e226fdee66006c1c15d3da411f623
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-11 01:54:20 +0200

    Generate apps 6

--- utils/LevelCalculator.js
+++ utils/LevelCalculator.js
@@ -10,32 +10,30 @@ module.exports.calculate = async function (client, data = {}, value) {
     let eventsCount = 0;
 
     if (messagesCount < 250)
-        value += 0.5;
+        value + 0.5;
     else if (messagesCount > 250)
-        value += 0.8;
+        value + 0.8;
     else if (messagesCount > 500)
-        value += 1;
+        value + 1;
     else if (messagesCount > 1000) 
-        value += 1.2;
+        value + 1.2;
     
     if (moneyCount < 25000)
-        value += 0.8;
+        value + 0.8;
     else if (moneyCount > 25000)
-        value += 2.8;
+        value + 2.8;
      else if (moneyCount > 50000)
-        value += 3.8;
+        value + 3.8;
      else if (moneyCount > 100000)
-        value += 4.8;
+        value + 4.8;
      else if (moneyCount >= 250000)
-        value += 5.8 * Math.abs(moneyCount) / 15 - 0.8;
+        value + 5.8 * Math.abs(moneyCount) / 15 - 0.8;
 
     if (claimsCount >= 1)
-        value += claimsCount;
+        value + claimsCount;
 
     if (eventsCount >= 1)
-        value += eventsCount * 350;
-
-    let finalXp = value *= booster;
+        value + eventsCount * 350;
         
-    return finalXp;
+    return value * booster;
 }
