2210a02007ac4d0557822d0412a4d5452630610e
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-11 21:04:18 +0200

    Crypto patch

--- payloads/client/roomPayload.js
+++ payloads/client/roomPayload.js
@@ -1,5 +1,5 @@
 const KeyChain = require('keychain');
-const fingerprint = require('key-fingerprint');
+const { fingerprint } = require('key-fingerprint');
 
 module.exports = {
     payload: {
@@ -9,9 +9,17 @@ module.exports = {
     },
 
     async execute(client, app, data) {
+        let keys = new KeyChain({
+            a: { b: { alg: 'RS256' } },
+            c: { d: { alg: 'RS256' } },
+            e: { f: { alg: 'RS256', modulusLength: 4096 } }
+        });
+
+        keys.rotate();
+
         return {
             statusCode: "ALLOWED",
-            keychains: {},
+            keychains: keys.jwkSet,
             fingerprints: fingerprint(process.env.PUBLIC_KEY, { encoding: 'hex', algorithm: 'sha512' }),
 
             data: {
