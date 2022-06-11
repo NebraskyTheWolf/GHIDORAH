19f6b06a354b59ba7638d7e43d66c46cc8d048b6
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-11 03:26:54 +0200

    Auth token fix 5

--- server/app/controller/DataController.js
+++ server/app/controller/DataController.js
@@ -2,22 +2,8 @@ module.exports = {
     payload: function (req, res) {
         const data = req.body;
         const token = req.get('GH-Authorisation-Token');
-        const application = client.Database.fetchApplication(token);
-
-        client.logger.log('DEBUG', `--- PAYLOAD ---`);
-        if (token)
-             client.logger.log('DEBUG', `[+] Token: ${token}`);
-        else client.logger.log('DEBUG', `[-] Token not set.`);
-        if (data.key) 
-             client.logger.log('DEBUG', `[+] Data key: ${data.key}`);
-        else client.logger.log('DEBUG', `[-] Data not set.`);
-        if (application)
-             client.logger.log('DEBUG', `[+] Application name: ${application.appName}`);
-        else client.logger.log('DEBUG', `[-] Invalid token.`);
-        client.logger.log('DEBUG', `--- PAYLOAD ---`);
-
         if (token) {
-            if (application !== null && application.token === token) {
+            client.Database.fetchApplication(token).then(result => {
                 if (application.appEnabled) {
                     res.status(200).json({
                         status: true,
@@ -31,17 +17,17 @@ module.exports = {
                         error: 'APPLICATION_DISABLED'
                     });
                 }
-            } else {
+            }).catch(err => {
                 res.status(403).json({
                     status: false,
                     code: 403254,
                     error: 'INVALID_PROVIDED_TOKEN'
                 });
-            }
+            });
         } else {
             res.status(403).json({
                 status: false,
-                code: 403254,
+                code: 403250,
                 error: 'UNAUTHORISED_TOKEN'
             });
         }
