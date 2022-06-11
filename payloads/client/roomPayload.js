798872a11e024e2345a9e426f314cd53faf12f90
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-11 21:02:03 +0200

    Adding handlers 9

--- payloads/client/roomPayload.js
+++ payloads/client/roomPayload.js
@@ -11,11 +11,7 @@ module.exports = {
     async execute(client, app, data) {
         return {
             statusCode: "ALLOWED",
-            keychains: new KeyChain({
-                a: { b: { alg: 'RS256' } },
-                c: { d: { alg: 'RS256' } },
-                e: { f: { alg: 'RS256', modulusLength: 2048 } }
-            }),
+            keychains: {},
             fingerprints: fingerprint(process.env.PUBLIC_KEY, { encoding: 'hex', algorithm: 'sha512' }),
 
             data: {
