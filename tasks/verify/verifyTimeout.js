e4bace75ae90c915dca1a2d64c09e93847a7d843
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-26 17:31:07 +0200

    Patching verification issues ( DUAL VERIFICATION FIX ) 13

--- tasks/verify/verifyTimeout.js
+++ tasks/verify/verifyTimeout.js
@@ -7,7 +7,7 @@ module.exports = {
         cronTime: 300000
     },
     execute() {
-        client.Database.getAllEntries().forEach(result => {
+        client.Database.getAllEntries().then(result => {
             if ((date - new Date(result.registeredAt) > ALIVE_ENTRY)) {
                 client.Database.deleteEntry(result.id).then(request => {
                     if (result.id === 'DEFAULT') return;
