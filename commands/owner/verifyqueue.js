4289639eeece9f819b1390ff594c99175b1d9a2c
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-26 18:10:01 +0200

    add command verifyqueue 3

--- commands/owner/verifyqueue.js
+++ commands/owner/verifyqueue.js
@@ -32,9 +32,14 @@ module.exports = {
             embed.addField(`<@${results.id}>`, `${moment(results.registeredAt)}`, false);
         });
 
-        await interaction.reply({
-            embeds: [embed],
-            ephemeral: true
+        client.api.interactions(interaction.id, interaction.token).callback.post({
+            data: {
+                type: 4,
+                data: {
+                    embeds: [embed],
+                    ephemeral: true
+                }
+            }
         });
     }
 }
