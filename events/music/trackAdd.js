c0adb735135bbbbb6c82c5352bce09ca0bd40545
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-28 20:00:35 +0200

    Adding music handler 6

--- events/music/trackAdd.js
+++ events/music/trackAdd.js
@@ -7,7 +7,24 @@ module.exports = function (client, queue, track) {
                   "components": [
                     {
                       "style": 2,
-                      "label": `Music added --> ${track.title}`,
+                      "label": `Music added -> ${track.title}`,
+                      "custom_id": `row_0_button_0`,
+                      "disabled": true,
+                      "emoji": {
+                        "id": `920840187230158848`,
+                        "name": `TFA_Music`,
+                        "animated": false
+                      },
+                      "type": 2
+                    }
+                  ]
+                },
+                {
+                  "type": 1,
+                  "components": [
+                    {
+                      "style": 2,
+                      "label": `Requested by ${queue.requestedBy.username}`,
                       "custom_id": `row_0_button_0`,
                       "disabled": true,
                       "emoji": {
