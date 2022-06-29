6598231fd0961328c9a3f6f449e47876e1d7a0ad
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-29 11:37:29 +0200

    Fix Access-Control mistakes 2

--- server/http.js
+++ server/http.js
@@ -37,7 +37,7 @@ module.exports = client => {
 	server.use(passport.session());
 
 	server.use(function (req, res, next) {
-		res.header('Access-Control-Allow-Origin', 'skf-studios.com');
+		res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Methods', 'GET,POST');
 		res.header('Access-Control-Allow-Headers', 'Content-Type');
 
