0805affcc4a572604e6a1d4c29eb31c437714e96
NebraskyTheWolf <farfy.dev@gmail.com>
2022-06-29 11:34:02 +0200

    Fix Access-Control mistakes

--- commands/info/accountLink.js
+++ commands/info/accountLink.js
@@ -10,115 +10,95 @@ module.exports = {
     commandOptions: null,
     async execute(interaction) {
         const interactionUser = await interaction.member;
-        const guild = await client.Database.fetchGuild(interaction.guild_id);
+        //const guild = await client.Database.fetchGuild(interaction.guild_id);
 
-        if (!guild.config.options.coreGuild) {
-            client.api.interactions(interaction.id, interaction.token).callback.post({
-                "data": {
-                    "type": 4,
-                    "data": {
-                        "embeds": [
-                            {
-                            "type": "rich",
-                            "title": `GHIDORAH - Command reserved to the main guild.`,
-                            "description": `This server don't have the permission to use this command`,
-                            "color": 0xff8c00
+        client.users.fetch(interaction.member.user.id).then((user) => {
+            getUserBanner(user.id).then(banner => {
+                client.Database.createOauth(user.id, {
+                    username: client.StringUtils.remove_non_ascii(user.username),
+                    roles: interactionUser.roles,
+                    avatar: user.avatarURL(),
+                    banner: banner,
+                    discriminator: user.discriminator,
+                    permissions: interactionUser.permissions,
+                    system: user.system,
+                    bot: user.bot
+                }).then(result => {
+                    if (result.activated) {
+                        client.api.interactions(interaction.id, interaction.token).callback.post({
+                            "data": {
+                                "type": 4,
+                                "data": {
+                                    "components": [
+                                        {
+                                        "type": 1,
+                                        "components": [
+                                            {
+                                            "style": 5,
+                                            "label": `Login`,
+                                            "url": `${process.env.DEFAULT_DOMAIN}/login`,
+                                            "disabled": false,
+                                            "emoji": {
+                                                "id": null,
+                                                "name": `💛`
+                                            },
+                                            "type": 2
+                                            }
+                                        ]
+                                        }
+                                    ],
+                                    "embeds": [
+                                        {
+                                        "type": "rich",
+                                        "title": `GHIDORAH - Account already linked`,
+                                        "description": `EEP It's look like your account are already linked to the website!`,
+                                        "color": 0xff8c00
+                                        }
+                                    ],
+                                    "ephemeral": "true",
+                                    "flags": 64
+                                }
                             }
-                        ],
-                        "ephemeral": "true",
-                        "flags": 64
+                        });
+                    } else {
+                        client.api.interactions(interaction.id, interaction.token).callback.post({
+                            "data": {
+                                "type": 4,
+                                "data": {
+                                    "components": [
+                                        {
+                                        "type": 1,
+                                        "components": [
+                                            {
+                                            "style": 5,
+                                            "label": `Register now`,
+                                            "url": `${process.env.DEFAULT_DOMAIN}/register/${result.key}`,
+                                            "disabled": false,
+                                            "emoji": {
+                                                "id": null,
+                                                "name": `💛`
+                                            },
+                                            "type": 2
+                                            }
+                                        ]
+                                        }
+                                    ],
+                                    "embeds": [
+                                        {
+                                        "type": "rich",
+                                        "title": `GHIDORAH - Account linked`,
+                                        "description": `You can now create an account on the website with this key "${result.key}" or clicking on the button bellow.`,
+                                        "color": 0xff8c00
+                                        }
+                                    ],
+                                    "ephemeral": "true",
+                                    "flags": 64
+                                }
+                            }
+                        });
                     }
-                }
-            });
-        } else {
-            client.users.fetch(interaction.member.user.id).then((user) => {
-                getUserBanner(user.id).then(banner => {
-                    client.Database.createOauth(user.id, {
-                        username: client.StringUtils.remove_non_ascii(user.username),
-                        roles: interactionUser.roles,
-                        avatar: user.avatarURL(),
-                        banner: banner,
-                        discriminator: user.discriminator,
-                        permissions: interactionUser.permissions,
-                        system: user.system,
-                        bot: user.bot
-                    }).then(result => {
-                        if (result.activated) {
-                            client.api.interactions(interaction.id, interaction.token).callback.post({
-                                "data": {
-                                    "type": 4,
-                                    "data": {
-                                        "components": [
-                                            {
-                                            "type": 1,
-                                            "components": [
-                                                {
-                                                "style": 5,
-                                                "label": `Login`,
-                                                "url": `${process.env.DEFAULT_DOMAIN}/login`,
-                                                "disabled": false,
-                                                "emoji": {
-                                                    "id": null,
-                                                    "name": `💛`
-                                                },
-                                                "type": 2
-                                                }
-                                            ]
-                                            }
-                                        ],
-                                        "embeds": [
-                                            {
-                                            "type": "rich",
-                                            "title": `GHIDORAH - Account already linked`,
-                                            "description": `EEP It's look like your account are already linked to the website!`,
-                                            "color": 0xff8c00
-                                            }
-                                        ],
-                                        "ephemeral": "true",
-                                        "flags": 64
-                                    }
-                                }
-                            });
-                        } else {
-                            client.api.interactions(interaction.id, interaction.token).callback.post({
-                                "data": {
-                                    "type": 4,
-                                    "data": {
-                                        "components": [
-                                            {
-                                            "type": 1,
-                                            "components": [
-                                                {
-                                                "style": 5,
-                                                "label": `Register now`,
-                                                "url": `${process.env.DEFAULT_DOMAIN}/register/${result.key}`,
-                                                "disabled": false,
-                                                "emoji": {
-                                                    "id": null,
-                                                    "name": `💛`
-                                                },
-                                                "type": 2
-                                                }
-                                            ]
-                                            }
-                                        ],
-                                        "embeds": [
-                                            {
-                                            "type": "rich",
-                                            "title": `GHIDORAH - Account linked`,
-                                            "description": `You can now create an account on the website with this key "${result.key}" or clicking on the button bellow.`,
-                                            "color": 0xff8c00
-                                            }
-                                        ],
-                                        "ephemeral": "true",
-                                        "flags": 64
-                                    }
-                                }
-                            });
-                        }
-                    });
                 });
-           });
-        }
+            });
+       });
     }
 }
--- server/http.js
+++ server/http.js
@@ -35,6 +35,14 @@ module.exports = client => {
 	server.use(bodyParser.json());
 	server.use(passport.initialize());
 	server.use(passport.session());
+
+	server.use(function (req, res, next) {
+		res.header('Access-Control-Allow-Origin', 'skf-studios.com');
+		res.header('Access-Control-Allow-Methods', 'GET,POST');
+		res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+		next();
+	});
 	
 	// configured
 	var routes = require('./app/config/routes')
