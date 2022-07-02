63301e0ec46d7206ecb6240b639cb8a271b3287c
NebraskyTheWolf <farfy.dev@gmail.com>
2022-07-03 00:47:13 +0200

    Debug 6

--- tasks/socials/YoutubeCheck.js
+++ tasks/socials/YoutubeCheck.js
@@ -10,15 +10,11 @@ module.exports = {
         await client.Database.getAllYoutubers().then(async youtubers => {
             console.log(youtubers[0])
             for (i = 0; i < youtubers.length; i++) {
-                console.log(`Data: ${youtubers[i]}`);
-                console.log(`DataServer: ${youtubers[i].guildId}`);
                 await client.Database.fetchGuild(youtubers[i].guildId).then(async guild => {
                     parser.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${youtubers[i].channelURL}`)
                     .then(async data => {
-                        console.log(data)
                         await client.Database.checkYoutubeVideo(guild.id, data.items[0].link)
                         .catch(async () => {
-                            console.log('')
                             await client.Database.createYoutubeVideo(guild.id, data.items[0].link)
                             .then(async finalVideo => {
                                 if (guild.socials.youtubeChannel) {
