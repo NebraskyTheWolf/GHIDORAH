94dbb132465bc5982084c95da1c6c9a615294929
NebraskyTheWolf <farfy.dev@gmail.com>
2022-07-03 04:37:40 +0200

    New /profile

--- commands/info/profile.js
+++ commands/info/profile.js
@@ -1,13 +1,118 @@
 const {Collection} = require("discord.js");
+const convertor = require('./utils/ImageHandler');
+const { v4 } = require('uuid');
+const { MessageEmbed } = require("discord.js");
+
+const rankCard = `<div class="ui container page-content">
+<h1 class="ui center aligned header">
+    <img src="{{ usericon }}" class="ui rounded staff image" alt="Vakea">
+    <div class="content">
+        {{ username }}
+    </div>
+</h1>
+<div class="ui divider"></div>
+    <div class="ui stackable grid" style="position:relative;">
+        <div class="ui eight wide column">
+            <h2 class="ui header">
+                 Profile
+                <div class="sub header"></div>
+            </h2>
+            <br>
+            <div class="ui four small statistics">
+                <div class="statistic">
+                    <div class="value">
+                        {{ level }}
+                    </div>
+                    <div class="label">
+                        Level
+                    </div>
+                </div>
+                <div class="statistic">
+                    <div class="value">
+                        {{ xp }}
+                    </div>
+                    <div class="label">
+                        Scores
+                    </div>
+                </div>
+                <div class="statistic">
+                    <div class="value">
+                        {{ rankname }}
+                    </div>
+                    <div class="label">
+                        Rank
+                    </div>
+                </div>
+                <div class="statistic">
+                    <div class="value">
+                        #{{ position }}
+                    </div>
+                    <div class="label">
+                        Position
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="ui indicating progress" id="levels" data-percent="89">
+                <div class="bar" style="transition-duration: 300ms; width: 89%;">
+                    <div class="progress">350/400</div>
+                </div>
+                <div class="label">Level</div>
+            </div>
+        </div>
+        <div class="ui vertical divider"></div>
+        <div class="ui eight wide column">
+            <h2 class="ui header">
+                Badges
+                <div class="sub header">All the badges of {{ username }}</div>
+            </h2>
+            </br>
+            <a class="ui yellow label">
+              Lurky cat
+              <div class="detail">Official lurker</div>
+            </a>
+            </br>
+            <a class="ui orange label">
+              Expert Stimky cat
+              <div class="detail">Good smell :3</div>
+            </a>
+            </br>
+            <a class="ui yellow label">
+              Expert chimken murder
+              <div class="detail">Official murder of chimkens</div>
+            </a>
+            </br>
+            <a class="ui purple label">
+               Expert arson cat
+              <div class="detail">Official arson</div>
+            </a>
+        </div>
+</div>
+</div>`;
 
 module.exports = {
     name: "profile",
     description: "See your game profile.",
     commandOptions: null,
     async execute(interaction) {
-        client.Database.fetchUser(interaction.member.user.id).then(async (uwu) => {
-          const user = await client.levels.fetch(interaction.member.user.id, interaction.guild_id, true);
-          client.api.interactions(interaction.id, interaction.token).callback.post({
+        const didUser = interaction.member.user;
+        const user = await client.levels.fetch(interaction.member.user.id, interaction.guild_id, true);
+        const id = v4();
+
+        convertor.generate(rankCard, { 
+              usericon: didUser.avatarURL(),
+              username: didUser.tag,
+              level: user.level,
+              xp: user.xp,
+              requiredXp: client.levels.xpFor(user.level + 1),
+              rankname: client.Modlog.fetchRankData(user.xp).name,
+              position: user.position,
+        }, result => {
+            const embed = new MessageEmbed()
+            .setTitle('Profile')
+            .setImage(`attachment://${id}.png`);
+
+            client.api.interactions(interaction.id, interaction.token).callback.post({
               "data": {
                   "type": 4,
                   "data": {
@@ -17,47 +122,24 @@ module.exports = {
                             "components": [
                               {
                                 "style": 5,
-                                "label": `Stats`,
+                                "label": `Profile`,
                                 "url": `${process.env.DEFAULT_DOMAIN}/server/${interaction.guild_id}/${uwu.id}/profile`,
                                 "disabled": false,
                                 "type": 2
                               }
                             ]
                           }
-                        ],
-                        "embeds": [
-                          {
-                            "type": "rich",
-                            "title": `GHIDORAH - Profile`,
-                            "description": `There is your game profile.`,
-                            "color": 0xff00cc,
-                            "fields": [
-                              {
-                                "name": `Level`,
-                                "value": `${user.level}`,
-                                "inline": true
-                              },
-                              {
-                                "name": `XP`,
-                                "value": `${user.xp}`,
-                                "inline": true
-                              },
-                              {
-                                "name": `Money`,
-                                "value": `${uwu.stats.stats.money}`,
-                                "inline": true
-                              },
-                              {
-                                "name": `Rank`,
-                                "value": `${client.Modlog.fetchRankData(user.xp).name} #${user.position}`,
-                                "inline": true
-                              }
-                            ]
-                          }
+                      ],
+                      "embeds": [embed],
+                      "files": [
+                        {
+                          "attachment": result.data,
+                          "file": `${id}.png`
+                        }
                       ]
                   }
               }
-          })
-      });
+          });
+        });
     }
 }
--- utils/ImageHandler.js
+++ utils/ImageHandler.js
@@ -1,5 +1,7 @@
 const nodeHtmlToImage = require('node-html-to-image');
 
+const { v4 } = require('uuid');
+
 /**
  * @function generate()
  * 
@@ -9,16 +11,59 @@ const nodeHtmlToImage = require('node-html-to-image');
  * 
  * @author NebraskyTheWolf
  */
-
 module.exports.generate = async function (html, content, callback) {
+    const id = v4();
     await nodeHtmlToImage({
-        html: html,
+        html: `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" href="https://skf-studios.com/css/semantic.min.css">
+            <link rel="stylesheet" href="https://skf-studios.com/css/search.min.css">
+            <link rel="stylesheet" href="https://skf-studios.com/css/transition.min.css">
+            <link rel="stylesheet" href="https://skf-studios.com/css/custom-responsive.css">
+            <link rel="stylesheet" href="https://skf-studios.com/css/app.css">
+        </head>
+        <body class="front">
+            <div class="pusher">
+                <div class="container">
+                    ${html}
+                </div>
+            </div>
+        
+            <script type="text/javascript" src="https://skf-studios.com/js/semantic.min.js"></script>
+            <script type="text/javascript" src="https://skf-studios.com/js/search.min.js"></script>
+            <script type="text/javascript" src="https://skf-studios.com/js/transition.min.js"></script>
+            <script type="text/javascript" src="https://skf-studios.com/js/app.js"></script>
+            <script type="text/javascript" src="https://skf-studios.com/js/form.js"></script>
+            <script type="text/javascript" src="https://skf-studios.com/js/jquery-3.2.1.min.js"></script>
+        
+            <script type="text/javascript">
+                $('#levels').progress({
+                    total: '400',
+                    value: '100',
+                    text: {
+                        percent: '100/400'
+                    }
+                })
+                $(document).ready(function () {
+                    $('[data-toggle="popup"]').each(function (k, el) {
+                        $(el).popup({
+                            html: $(el).attr('data-content'),
+                            position: $(el).attr('data-placement'),
+                            variation: $(el).attr('data-variation')
+                        })
+                    })
+                })
+            </script>
+        </body>
+        </html>`,
         transparent: true,
         type: 'png',
         quality: 100,
         content: content,
-        output: 'rankcard.png'
+        output: `./assets/build/${id}.png`
     })
-    .then(result => callback({ status: true, data: result }))
+    .then(result => callback({ status: true, data: result, path: `./assets/build/${id}.png` }))
     .catch(result => callback({ status: false, data: result }));
 }
