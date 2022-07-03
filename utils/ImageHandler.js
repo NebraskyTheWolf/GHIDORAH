9fbc91a3917fc2b770f6fd1ded43ee5b03de9c67
NebraskyTheWolf <farfy.dev@gmail.com>
2022-07-03 06:57:14 +0200

    switching firefox 6

--- utils/ImageHandler.js
+++ utils/ImageHandler.js
@@ -64,7 +64,7 @@ module.exports.generate = async function (html, content, callback) {
         content: content,
         puppeteerArgs: {
             args: ['--no-sandbox'],
-            defaultViewport: { width: 785, height: 425},
+            defaultViewport: { width: 785, height: 520},
             timeout: 3000
         }
     })
