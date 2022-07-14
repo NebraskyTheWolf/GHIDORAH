7a05be08cf75ae7bb86f417d1bab6c826306e45f
NebraskyTheWolf <matazox302@gmail.com>
2022-07-14 20:12:40 +0200

    NeyazR1 update6

--- buttons/neyaz/row_neyaz_price_info.js
+++ buttons/neyaz/row_neyaz_price_info.js
@@ -8,8 +8,46 @@ module.exports = {
         switch ( value ) {
             case 'row_neyaz_solo': {
                 interaction.reply({
-                    content: 'Not implemented yet',
-                    flags: 64
+                    "components": [
+                        {
+                          "type": 1,
+                          "components": [
+                            {
+                              "style": 3,
+                              "label": `Je réserve une place!`,
+                              "custom_id": `row_neyaz_reservation`,
+                              "disabled": true,
+                              "type": 2
+                            }
+                          ]
+                        }
+                      ],
+                      "embeds": [
+                        {
+                          "type": "rich",
+                          "title": `Formule Solo`,
+                          "description": `Comment ce déroule la session de coaching?\n```\nEn stand de tire ou je montre plein de petit tips j'aide sur les lacune \nde chacun et je donne plusieurs petit conseille important beaucoup \nde travaillé sur les pick et overpick\n````,
+                          "color": 0xe1902d,
+                          "fields": [
+                            {
+                              "name": `Temps`,
+                              "value": `**45 min â 1h10**`,
+                              "inline": true
+                            },
+                            {
+                              "name": `Prix`,
+                              "value": `**15€**`,
+                              "inline": true
+                            },
+                            {
+                              "name": `Type`,
+                              "value": `**SOLO**`,
+                              "inline": true
+                            }
+                          ]
+                        }
+                    ],
+                    "flags": 64
                 });
             }
             break;
