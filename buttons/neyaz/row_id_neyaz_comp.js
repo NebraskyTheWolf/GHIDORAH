b1e5f7936fcf2a5d40751354c41f92d33bd93b03
NebraskyTheWolf <matazox302@gmail.com>
2022-07-14 19:35:36 +0200

    NeyazR1 adding impl

---
+++ buttons/neyaz/row_id_neyaz_comp.js
@@ -0,0 +1,11 @@
+module.exports = {
+    data: {
+        name: "row_id_neyaz_comp"
+    },
+    async execute(interaction, interactionUser) {
+        interaction.reply({
+            content: 'Not implemented yet',
+            flags: 64
+        });
+    }
+}
---
+++ buttons/neyaz/row_id_neyaz_solo.js
@@ -0,0 +1,11 @@
+module.exports = {
+    data: {
+        name: "row_id_neyaz_solo"
+    },
+    async execute(interaction, interactionUser) {
+        interaction.reply({
+            content: 'Not implemented yet',
+            flags: 64
+        });
+    }
+}
