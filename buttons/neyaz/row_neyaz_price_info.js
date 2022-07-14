d5c49660b4e61fda8f1826637693582603453f4c
NebraskyTheWolf <matazox302@gmail.com>
2022-07-14 20:02:10 +0200

    NeyazR1 update5

--- buttons/neyaz/row_neyaz_price_info.js
+++ buttons/neyaz/row_neyaz_price_info.js
@@ -3,6 +3,33 @@ module.exports = {
         name: "row_neyaz_price_info"
     },
     async execute(interaction, interactionUser) {
-        console.log(interaction);
+        const value = interaction.values[0];
+
+        switch ( value ) {
+            case 'row_neyaz_solo': {
+                interaction.reply({
+                    content: 'Not implemented yet',
+                    flags: 64
+                });
+            }
+            break;
+            case 'row_neyaz_comp': {
+                interaction.reply({
+                    content: 'Not implemented yet',
+                    flags: 64
+                });
+            }
+            break;
+            case 'row_neyaz_ranked': {
+                interaction.reply({
+                    content: 'Not implemented yet',
+                    flags: 64
+                });
+            }
+            break;
+            default:
+                console.log('Invalid key for Neyaz func.');
+            break;
+        }
     }
 }
