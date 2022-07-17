c8604ae159416a256bb35ce3bf3ecfef82064437
NebraskyTheWolf <matazox302@gmail.com>
2022-07-17 17:57:44 +0200

    Beta test structures Fix 2

--- core/handlers/payload.js
+++ core/handlers/payload.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 module.exports = client => {
-	fs.readdir("./payloads/client/", (err, files) => {
+	fs.readdir("../components/payloads/client/", (err, files) => {
 		if (err) console.log(err);
 		files.forEach(file => {
-			const payload = require(`../components/payloads/client/${file}`);
+			const payload = require(`../payloads/client/${file}`);
 			client.payload.set(payload.payload.key, payload);
 		});
 	});
--- core/handlers/security.js
+++ core/handlers/security.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 module.exports = client => {
-	fs.readdir("./security/api/", (err, files) => {
+	fs.readdir("../components/security/api/", (err, files) => {
 		if (err) console.log(err);
 		files.forEach(file => {
-			const middleware = require(`../components/security/api/${file}`);
+			const middleware = require(`../security/api/${file}`);
 			client.payload.set(middleware.middleware.key, middleware);
 		});
 	});
