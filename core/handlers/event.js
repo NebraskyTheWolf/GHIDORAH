9b8d4b5f75e4656a7bda66fbd5008216cdc5481c
NebraskyTheWolf <matazox302@gmail.com>
2022-07-17 18:23:13 +0200

    Beta test structures Fix 16

--- core/handlers/event.js
+++ core/handlers/event.js
@@ -5,7 +5,7 @@ module.exports = client => {
 		console.log(files)
 		console.log(folders)
         const folder = fs
-			.readdirSync(`./events/${files}/`)
+			.readdirSync(`./${files}/`)
 			.filter(file => file.endsWith(".js")); 
             for (const commands of folder) {
 
