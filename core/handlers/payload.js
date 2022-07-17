ebaf0d94d03d4e99183d5872cc23d90c565bc033
NebraskyTheWolf <matazox302@gmail.com>
2022-07-17 17:58:57 +0200

    Beta test structures Fix 3

--- core/handlers/payload.js
+++ core/handlers/payload.js
@@ -4,7 +4,7 @@ module.exports = client => {
 	fs.readdir("../components/payloads/client/", (err, files) => {
 		if (err) console.log(err);
 		files.forEach(file => {
-			const payload = require(`../payloads/client/${file}`);
+			const payload = require(`./payloads/client/${file}`);
 			client.payload.set(payload.payload.key, payload);
 		});
 	});
