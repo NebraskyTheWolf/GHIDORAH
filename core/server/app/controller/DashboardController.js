ead2015e325b2ff432ecfc1998f59ab4f8b018ed
NebraskyTheWolf <matazox302@gmail.com>
2022-07-23 01:24:52 +0200

    Add dashboard controller.

---
+++ core/server/app/controller/DashboardController.js
@@ -0,0 +1,9 @@
+module.exports = {
+    fetchLogs: function (req, res) {},
+    pushLogs: function (req, res) {},
+
+    modInfo: function (req, res) {},
+    modLogin: function (req, res) {},
+
+    payload: function (req, res) {},
+}
