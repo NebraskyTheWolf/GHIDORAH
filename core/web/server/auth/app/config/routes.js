046af4fdce62c2b982c092e6c3bc0f7d66adef5e
NebraskyTheWolf <matazox302@gmail.com>
2022-08-09 22:34:06 +0100

    Fix auth api erros

--- core/web/server/auth/app/config/routes.js
+++ core/web/server/auth/app/config/routes.js
@@ -1,17 +1,17 @@
 module.exports = {
-    'get /': {'function': 'ApiController.fetch', protected: false},
-    'get /status': {'function': 'ApiController.fetchStatus', protected: false},
+    'get /auth/': {'function': 'ApiController.fetch', protected: false},
+    'get /auth/status': {'function': 'ApiController.fetchStatus', protected: false},
 
-    'post /user': {'function': 'UserController.fetchUser', protected: true},
-    'post /user/search': {'function': 'UserController.search', protected: true},
+    'post /auth/user': {'function': 'UserController.fetchUser', protected: true},
+    'post /auth/user/search': {'function': 'UserController.search', protected: true},
 
-    'post /user/sessions': {'function': 'UserController.fetchHistory', protected: true},
-    'post /user/sessions/revoke': {'function': 'UserController.revokeSession', protected: true},
-    'post /user/sessions/create': {'function': 'UserController.createHistory', protected: true},
+    'post /auth/user/sessions': {'function': 'UserController.fetchHistory', protected: true},
+    'post /auth/user/sessions/revoke': {'function': 'UserController.revokeSession', protected: true},
+    'post /auth/user/sessions/create': {'function': 'UserController.createHistory', protected: true},
 
-    'post /user/terminate': {'function': 'UserController.terminateSession', protected: true},
-    'post /user/edit': {'function': 'UserController.editUser', protected: true},
-    'post /user/delete': {'function': 'UserController.deleteUser', protected: true},
+    'post /auth/user/terminate': {'function': 'UserController.terminateSession', protected: true},
+    'post /auth/user/edit': {'function': 'UserController.editUser', protected: true},
+    'post /auth/user/delete': {'function': 'UserController.deleteUser', protected: true},
 
     'post /auth/login': {'function': 'AuthController.authenticate', protected: false},
     'post /auth/register': {'function': 'AuthController.register', protected: false},
